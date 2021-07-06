7d33bb98d9aca06b6fb73ab5af3359bed8853464
mwezyk <maciek.wezyk@gmail.com>
2021-07-06 23:49:49 +0200

    Added more customers to store in database

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -2,4 +2,9 @@ INSERT INTO customers (id, cust_name, balance)
 VALUES
        (1, 'John Doe', 1000000000),
        (2, 'Harry Kane', 70000000),
-       (3, 'Luke Shaw', 500000000);
+       (3, 'Luke Shaw', 500000000),
+       (4, 'Diamond Hand', 0),
+       (5, 'Impossible Trader', -100),
+       (6, 'Alvaro Morata', -99999999),
+       (7, 'Jorginho', 999999999),
+       (7, 'Franklin Clinton', 999999999999);
