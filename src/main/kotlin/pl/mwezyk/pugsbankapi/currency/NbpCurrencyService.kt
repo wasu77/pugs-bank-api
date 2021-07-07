fe91d64c2a41924e8a10dbca0461eb90752c168e
mwezyk <maciek.wezyk@gmail.com>
2021-07-07 13:23:02 +0200

    Fixed converting currency for amounts lower than 100 Groszy

--- src/main/kotlin/pl/mwezyk/pugsbankapi/currency/NbpCurrencyService.kt
+++ src/main/kotlin/pl/mwezyk/pugsbankapi/currency/NbpCurrencyService.kt
@@ -11,11 +11,20 @@ class NbpCurrencyService(currencyClient: NbpCurrencyClient) : CurrencyService {
     private val client = currencyClient;
 
     override fun convertToCurrency(amount: Long, currencyCode: CurrencyCode): String {
-        val inDollars = amount.div(CENTS_IN_DOLLAR).div(getExchangeRate())
+        val inDollars = getFormattedAmount(amount)
+        return getToDollarsFormatter().format(inDollars)
+    }
+
+    private fun getFormattedAmount(amount: Long) = if (amount in (-100 .. 100))
+        amount.div(getExchangeRate()).div(CENTS_IN_DOLLAR)
+    else
+        amount.div(CENTS_IN_DOLLAR).div(getExchangeRate())
+
+    private fun getToDollarsFormatter(): NumberFormat {
         val format = NumberFormat.getCurrencyInstance(Locale.US)
-            format.maximumFractionDigits = 2
-            format.currency = Currency.getInstance(CurrencyCode.USD.name)
-        return format.format(inDollars)
+        format.maximumFractionDigits = 2
+        format.currency = Currency.getInstance(CurrencyCode.USD.name)
+        return format;
     }
 
     override fun getExchangeRate(currencyCode: CurrencyCode): Double {
