08d9b71c87c643eef75fa7547d546f47c15a3e03
mwezyk <maciek.wezyk@gmail.com>
2021-07-07 09:04:45 +0200

    Added simple swagger configuration

--- src/main/kotlin/pl/mwezyk/pugsbankapi/config/RestTemplateConfig.kt
+++ src/main/kotlin/pl/mwezyk/pugsbankapi/config/RestTemplateConfig.kt
@@ -1,4 +1,4 @@
-package pl.mwezyk.pugsbankapi.currency
+package pl.mwezyk.pugsbankapi.config
 
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
--- src/main/kotlin/pl/mwezyk/pugsbankapi/customer/CustomerController.kt
+++ src/main/kotlin/pl/mwezyk/pugsbankapi/customer/CustomerController.kt
@@ -1,5 +1,7 @@
 package pl.mwezyk.pugsbankapi.customer
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,13 +9,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("customers")
+@Api
 class CustomerController(val customerService: CustomerService) {
 
+    @ApiOperation(value = "Get list of all customers")
     @GetMapping
     fun getCustomers(): List<Customer> {
         return customerService.getAllCustomers()
     }
 
+    @ApiOperation(value = "Get customer with provided customerId")
     @GetMapping("/{customerId}")
     fun getCustomer(@PathVariable customerId: String): Customer {
         return customerService.getCustomerById(customerId.toLong())
