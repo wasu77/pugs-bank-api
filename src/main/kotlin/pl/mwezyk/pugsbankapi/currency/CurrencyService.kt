513979fa90483b73fb9a878daacd058b407ecc8f
mwezyk <maciek.wezyk@gmail.com>
2021-07-14 10:39:15 +0200

    Started using BigDecimal as money representation

--- src/main/kotlin/pl/mwezyk/pugsbankapi/currency/CurrencyService.kt
+++ src/main/kotlin/pl/mwezyk/pugsbankapi/currency/CurrencyService.kt
@@ -1,6 +1,6 @@
 package pl.mwezyk.pugsbankapi.currency
 
 interface CurrencyService {
-    fun getExchangeRate(currencyCode: CurrencyCode = CurrencyCode.USD) : Double
+    fun getExchangeRate(currencyCode: CurrencyCode = CurrencyCode.USD) : String
     fun convertToCurrency(amount: Long, currencyCode: CurrencyCode = CurrencyCode.USD) : String
 }
